13692598b7ad807ce71adb9905b7509095f939b5
IlhamF4 <52096634+IlhamF4@users.noreply.github.com>
2025-05-30 09:44:27 +0700

    Add function to play the game

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = "";
     const val = Math.floor(Math.random() * 3);
@@ -16,7 +19,29 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
-console.log()
+function playRound(humanChoice, computerChoice) {
+    let h = humanChoice;
+    let c = computerChoice;
+    
+    if (h === c) {
+        console.log("Draw!");
+    } else if (h === "paper" && c === "rock" || 
+        h === "rock" && c === "scissors" || 
+        h === "scissors" && c === "paper"){
+        console.log(`You win! ${h} beats ${c}`);
+        humanScore++;    
+    } else {
+        console.log(`You lose! ${c} beats ${h}`);
+        computerScore++;
+    }
+}
+
+for (let i = 0; i <5; i++){
+    let c = getComputerChoice()
+    let h = getHumanChoice()
+    playRound(h, c);
+}
+
+console.log(`Human score: ${humanScore}`);
+console.log(`Computer score: ${computerScore}`);
 
